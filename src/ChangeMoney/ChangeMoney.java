82e7c8db9d284422e7f34f18dc0e07bebafc8728
poemyoung <2427389569@qq.com>
2020-05-04 14:03:05 +0800

    5.04

---
+++ src/ChangeMoney/ChangeMoney.java
@@ -0,0 +1,40 @@
+package ChangeMoney;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 兑换零钱
+ */
+public class ChangeMoney {
+    public int coinChange(int[] coins, int amount) {
+        //动态规划数组
+        int INF = 1;
+        int[] dp = new int[amount+1];
+        //只能从0更新起走
+        Arrays.fill(dp,-INF);
+        dp[0] = 0;
+        //特殊情况处理
+        if(coins.length == 0){
+            return 0;
+        }
+        //循环赋值
+        for (int i = 0;i < coins.length;i++){
+            //更新
+            for (int j = coins[i];j <= amount;j++){
+                if(dp[j-coins[i]]!=-1) {
+                    dp[j] = dp[j] == -1?dp[j-coins[i]]+1 : Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {186,419,83,408};
+        int amount = 6249;
+        ChangeMoney test = new ChangeMoney();
+        System.out.println(test.coinChange(coins,amount));
+    }
+}
+
